e83f4fce1b737d2010c940e312008d8f3b3c94c1
milton munoz <livemilton@hotmail.com>
2025-02-05 22:11:13 -0500

    Pagination - Update Product List component for pagination

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -16,11 +16,19 @@ export class ProductListComponent {
   //add currente category Id
   currentCategoryId:number=1;
 
+  //add previous category to pagination
+  previousCategoryId: number=1;
+
   //Update component to read the category name
   currentCategoryName: string = "";
 
   //add boolean search
   searchMode: boolean=false;
+
+  //new properties for pagination
+  thePageNumber: number =1;
+  thePageSize: number =10;
+  theTotalElements: number =0;
 
 
   constructor(private productService: ProductService,
@@ -81,12 +89,32 @@ export class ProductListComponent {
       this.currentCategoryName = 'Books';
     }
 
+    //
+    //Check if we have a different category than previous
+    //
+
+    //if we have a different category id than previous
+    //then set thePageNumber back to 1
+
+    if(this.previousCategoryId != this.currentCategoryId){
+      this.thePageNumber=1;
+    }
+    this.previousCategoryId=this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
     //now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data=>{
-        this.products=data;
-      }
-    )
+    this.productService.getProductListPaginate(this.thePageNumber -1,
+                                              this.thePageSize,
+                                              this.currentCategoryId)
+                                              .subscribe(
+                                                data=>{
+                                                  this.products = data._embedded.products;
+                                                  this.thePageNumber = data.page.number+1;
+                                                  this.thePageSize = data.page.size;
+                                                  this.theTotalElements = data.page.totalElements;
+                                                }
+                                              );
 
   }
 
