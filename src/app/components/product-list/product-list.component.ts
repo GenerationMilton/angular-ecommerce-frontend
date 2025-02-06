f8cb5b5ddf5e4f2e96d24dae252832fd698a6c94
milton munoz <livemilton@hotmail.com>
2025-02-05 23:00:32 -0500

    Pagination -Pagination Demo and fix issues

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -26,7 +26,7 @@ export class ProductListComponent {
 
   //new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(
