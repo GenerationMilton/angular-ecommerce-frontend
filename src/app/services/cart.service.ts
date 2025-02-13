22e4e5e69d620f382d8dcfb6f99a8a5ccaf089d2
milton munoz <livemilton@hotmail.com>
2025-02-12 21:10:50 -0500

    shopping cart status-compute totals

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,58 +3,75 @@ import { CartItem } from '../common/cart-item';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   //cartItem array
-  cartItems: CartItem[]=[];
+  cartItems: CartItem[] = [];
 
   //total price and total quantity objects
   totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number>= new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
 
-  constructor() { }
+  constructor() {}
 
   //method add to cart to pass item
-  addToCart(theCartItem: CartItem){
-
+  addToCart(theCartItem: CartItem) {
     //check if we already have the item in our cart
     let alreadyExistsInCart: boolean = false;
     let existingCartItem: CartItem = undefined!;
 
-    if(this.cartItems.length > 0){
+    if (this.cartItems.length > 0) {
       //find the item in the cart based on item id
 
-      for(let tempCartItem of this.cartItems){
-        if(tempCartItem.id=== theCartItem.id){
-          existingCartItem= tempCartItem;
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
+          existingCartItem = tempCartItem;
           break;
         }
       }
 
       //check if we found it
-      alreadyExistsInCart =(existingCartItem != undefined);
+      alreadyExistsInCart = existingCartItem != undefined;
 
-      if(alreadyExistsInCart){
+      if (alreadyExistsInCart) {
         //increment the quantity
         existingCartItem.quantity++;
-      } else{
+      } else {
         //just add the item to the array
         this.cartItems.push(theCartItem);
       }
 
-      //compute cart total price and total wuantity
+      //compute cart total price and total quantity
       this.computeCartTotals();
-
-
     }
-    
-
-   
-
-
   }
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
 
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice!;
+      totalQuantityValue += currentCartItem.quantity;
+    }
 
+    //publish the new values ... all subscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    //log cart data just for debuggin purposes
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    for (let tempCartItem of this.cartItems) {
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice!;
+      console.log(
+        `name: ${tempCartItem.name}, quantity=${tempCartItem.quantity}, unitPrice=${tempCartItem.unitPrice}, subTotalPrice=${subTotalPrice}`
+      );
+    }
+
+    //Two digits after decimal
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);   
+    console.log('----'); 
+  }
 }
