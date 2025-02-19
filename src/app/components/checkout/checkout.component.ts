2364ce07afc4f1f0bb5d2ecfd5cee0f0c3f6f54f
milton munoz <livemilton@hotmail.com>
2025-02-18 19:29:14 -0500

    checkout from layout- fix issues

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -9,7 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CheckoutComponent implements OnInit{
 
 
-  checkoutFromGroup!: FormGroup;
+  checkoutFormGroup!: FormGroup;
 
   totalPrice: number=0;
   totalQuantity: number=0;
@@ -19,7 +19,7 @@ export class CheckoutComponent implements OnInit{
   ngOnInit(): void{
     
     //formgroup
-    this.checkoutFromGroup =this.formBuilder.group({
+    this.checkoutFormGroup =this.formBuilder.group({
 
       customer: this.formBuilder.group({
         firstName:[''],
@@ -54,22 +54,23 @@ export class CheckoutComponent implements OnInit{
     });
   }
 
-  //checkbox to validate and copy
-  copyShippingAddressToBillingAddress(event: Event): void {
-    if(event.target!.checked){
-      this.checkoutFromGroup.controls['billingAddress']
-        .setValue(this.checkoutFromGroup.controls['shippingAddress'].value);
-      }
-      else {
-        this.checkoutFromGroup.controls['billingAddress'].reset();
-      }
+  copyShippingAddressToBillingAddress(event: any) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+            .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
     }
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+    
+  }
 
   //method to purchase
   onSubmit(){
     console.log("handling the submit button");
-    console.log(this.checkoutFromGroup.get('customer')!.value);
-    console.log("The email address is "+ this.checkoutFromGroup.get('customer')!.value.email);
+    console.log(this.checkoutFormGroup.get('customer')!.value);
+    console.log("The email address is "+ this.checkoutFormGroup.get('customer')!.value.email);
   }
 
  
