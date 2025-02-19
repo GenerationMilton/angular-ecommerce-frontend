b8ba8b5d7170d40928d8085780894d1ff897a74b
milton munoz <livemilton@hotmail.com>
2025-02-18 21:16:11 -0500

    checkout form -validation to year and month

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -5,104 +5,118 @@ import { LivemiltonShopFormService } from 'src/app/services/livemilton-shop-form
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
-export class CheckoutComponent implements OnInit{
-
-
+export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
 
-  totalPrice: number=0;
-  totalQuantity: number=0;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
   //properties to year and months
-  creditCardYears: number[]=[];
-  creditCardMonths: number[]=[];
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
 
-  constructor( private formBuilder: FormBuilder,
-                private livemiltonShopFormservice: LivemiltonShopFormService){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private livemiltonShopFormservice: LivemiltonShopFormService
+  ) {}
 
-  ngOnInit(): void{
-    
+  ngOnInit(): void {
     //formgroup
-    this.checkoutFormGroup =this.formBuilder.group({
-
+    this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName:[''],
-        lastName:[''],
-        email:['']
+        firstName: [''],
+        lastName: [''],
+        email: [''],
       }),
       shippingAddress: this.formBuilder.group({
-        country:[''],
-        street:[''],
-        city:[''],
-        state:[''],
-        zipCode:[''],
-
+        country: [''],
+        street: [''],
+        city: [''],
+        state: [''],
+        zipCode: [''],
       }),
       billingAddress: this.formBuilder.group({
-        country:[''],
-        street:[''],
-        city:[''],
-        state:[''],
-        zipCode:[''],
-
+        country: [''],
+        street: [''],
+        city: [''],
+        state: [''],
+        zipCode: [''],
       }),
       creditCard: this.formBuilder.group({
-        cardType:[''],
-        nameOnCard:[''],
-        cardNumber:[''],
-        securityCode:[''],
-        expirationMonth:[''],
-        expirationYear:[''],
-
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
       }),
     });
 
     //populate credit card months
 
-    const startMonth: number= new Date().getMonth()+1;
-    console.log("startMonth: "+ startMonth);
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log('startMonth: ' + startMonth);
 
-    this.livemiltonShopFormservice.getCreditCardMonths(startMonth).subscribe(
-      data=>{
-        console.log("Retrieved credit card months: "+ JSON.stringify(data));
-        this.creditCardMonths=data;
-      }
-    )
-
+    this.livemiltonShopFormservice
+      .getCreditCardMonths(startMonth)
+      .subscribe((data) => {
+        console.log('Retrieved credit card months: ' + JSON.stringify(data));
+        this.creditCardMonths = data;
+      });
 
     //populate credit card years
 
-    this.livemiltonShopFormservice.getCreditCardyears().subscribe(
-      data=>{
-        console.log("Retrieved credit card years: "+ JSON.stringify(data));
-        this.creditCardYears=data;
-      }
-    )
-
-
-
+    this.livemiltonShopFormservice.getCreditCardyears().subscribe((data) => {
+      console.log('Retrieved credit card years: ' + JSON.stringify(data));
+      this.creditCardYears = data;
+    });
   }
 
   copyShippingAddressToBillingAddress(event: any) {
-
     if (event.target.checked) {
-      this.checkoutFormGroup.controls['billingAddress']
-            .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
-    }
-    else {
+      this.checkoutFormGroup.controls['billingAddress'].setValue(
+        this.checkoutFormGroup.controls['shippingAddress'].value
+      );
+    } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
-    
   }
 
   //method to purchase
-  onSubmit(){
-    console.log("handling the submit button");
+  onSubmit() {
+    console.log('handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')!.value);
-    console.log("The email address is "+ this.checkoutFormGroup.get('customer')!.value.email);
+    console.log(
+      'The email address is ' +
+        this.checkoutFormGroup.get('customer')!.value.email
+    );
   }
 
- 
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(
+      creditCardFormGroup?.value.expirationYear
+    );
+
+    //if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.livemiltonShopFormservice
+      .getCreditCardMonths(startMonth)
+      .subscribe((data) => {
+        console.log('Retrieved credit card monts: ' + JSON.stringify(data));
+        this.creditCardMonths = data;
+      });
+  }
 }
