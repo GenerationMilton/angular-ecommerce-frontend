8fd6b802894e06298aa8d320b11719beff8c82f1
milton munoz <livemilton@hotmail.com>
2025-02-24 10:35:31 -0500

    checkout form-custom validation rule - define the rule

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { LivemiltonShopFormService } from 'src/app/services/livemilton-shop-form.service';
+import { LivemiltonValidators } from 'src/app/validators/livemilton-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -37,10 +38,16 @@ export class CheckoutComponent implements OnInit {
     //formgroup
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('',[Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('',[Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('',
+                        [Validators.required, 
+                         Validators.minLength(2),
+                         LivemiltonValidators.notOnlyWhitespace]),
+        lastName: new FormControl('',
+                        [Validators.required, 
+                         Validators.minLength(2)]),
         email: new FormControl('',
-                              [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+                        [Validators.required,
+                         Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
         ),
       }),
       shippingAddress: this.formBuilder.group({
