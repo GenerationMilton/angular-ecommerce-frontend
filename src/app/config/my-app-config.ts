b9747fbbf258402a16b3e13fd2733b160222800b
milton munoz <livemilton@hotmail.com>
2025-03-03 14:24:37 -0500

    security-login-developApp configuration

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc:{
+        clientId:'zhnBIEEXxDSkL68AvVRDZ86FnWX1XyGd',
+        issuer:'https://dev-gpyjg06orv0bqjfe.us.auth0.com/oauth2/default',
+        redirectUri:'http://localhost:4200/login/callback',
+        scopes:['openid','profile','email']
+    }
+}
