fd08abf0393c3ccd4b6dc572defade3d8f14097a
milton munoz <livemilton@hotmail.com>
2025-03-03 15:02:18 -0500

    security-login-render th sign-In Widget

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -29,6 +29,20 @@ export class LoginComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: 'okta-sign-in-widget'}, //this name should be same as div tag in login.component.html
+      (response: any)=>{
+        if(response.status === 'SUCCESS'){
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error: any)=>{
+        throw error;
+      }
+    );
+
   }
 
 }
